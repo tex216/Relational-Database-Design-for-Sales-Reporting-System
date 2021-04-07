77aedd6b1de9622761f32c3f4e21a6ddbfd5d5ee
Runxin Zhang <moirobinzhang@users.noreply.github.com>
2021-04-07 15:01:38 -0400

    report 3 sql
    report 3 sql

--- rzhang609/LSRS/reporting/reports/report3.py
+++ rzhang609/LSRS/reporting/reports/report3.py
@@ -1,7 +1,4 @@
-from django.views import generic
 from django.shortcuts import render
-from django.db import connection
-from django.http import JsonResponse
 from reporting.forms import Report3SelectStateForm
 from reporting.repository.sqlhelper import SqlHelper
 
@@ -28,31 +25,27 @@ def store_revenue(request):
 
         return render(request, 'reporting/report3_store_revenue_get_state.html', context)
     elif request.method == "POST":
-
         form = Report3SelectStateForm(request.POST)
-        print ("touch post")
+
         if form.is_valid():
             state_location = request.POST.get('city_state_location')
             print(state_location)
             try:
                 obj = SqlHelper()
                 report3 = obj.report3_store_revenue_by_year_by_state(state_location)
-
-                print(report3)
                 obj.close()
+                print(report3)
                 context = {
                     'report3': report3,
                     'state_list': [],
                     'message': message,
                     'status': status,
                 }
-
-
                 return render(request, 'reporting/report3_store_revenue.html', context)
-
             except Exception as e:
                 message = "Exception {0} happens when selecting {1}. " \
                           "please refresh page".format(str(e), state_location)
+                print(message)
                 status = "failed"
                 context = {
                     'state_list': [],
