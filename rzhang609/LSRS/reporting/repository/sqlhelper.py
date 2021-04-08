5930c60048cf33d1ccf0ce8917df5007a1d85066
Runxin Zhang <moirobinzhang@users.noreply.github.com>
2021-04-08 15:14:08 -0400

    merge report 1, 2, 3, 4 and 6
    merge report 1, 2, 3, 4 and 6

--- rzhang609/LSRS/reporting/repository/sqlhelper.py
+++ rzhang609/LSRS/reporting/repository/sqlhelper.py
@@ -92,6 +92,44 @@ class SqlHelper(object):
                             "LIMIT 1;", [holiday_date, holiday_name, holiday_date, holiday_name])
         self.conn.commit()
 
+    # report 1 Category:  get data set
+    def get_report1(self):
+        self.cursor.execute(
+            "SELECT C.Category_Name AS Cate_Name, COUNT(P.PID) AS Cnt_Product, MIN(P.Retail_Price) AS Min_RtlPrc, "
+            "AVG(P.Retail_Price) AS Avg_RtlPrc, MAX(P.Retail_Price) AS Max_RtlPrc "
+            "FROM CATEGORY AS C "
+            "LEFT JOIN ASSIGNED AS A ON C.Category_Name = A.Category_Name "
+            "LEFT JOIN PRODUCT AS P ON A.PID = P.PID "
+            "GROUP BY C.Category_Name "
+            "ORDER BY C.Category_Name ASC; "
+        )
+        report1_res = self.cursor.fetchall()
+        return report1_res
+
+    # report 2 Actual versus Predicted Revenue for Couches and Sofas: get data set
+    def get_report2(self):
+        self.cursor.execute(
+            "SELECT P.PID AS PID, P.Product_Name AS Name, P.Retail_Price AS Price, "
+            "SUM(IFNULL(S.Quantity,0)) AS Tot_UnitSold, "
+            "SUM(IF(D.Discount_Price IS NULL,0,1) * IFNULL(S.Quantity,0)) AS Tot_UnitSold_AtDsct, "
+            "SUM(IF(D.Discount_Price IS NULL,1,0) * IFNULL(S.Quantity,0)) AS Tot_UnitSold_AtRtl, "
+            "SUM(IFNULL(S.Total_Amount,0)) AS Act_Revenue, "
+            "SUM(P.Retail_Price * IFNULL(S.Quantity,0) * IF(D.Discount_Price IS NULL, 1, 0.75)) AS Pred_Revenue, "
+            "(SUM(IFNULL(S.Total_Amount,0)) - SUM(P.Retail_Price * IFNULL(S.Quantity,0) * "
+            "IF(D.Discount_Price IS NULL, 1, 0.75))) AS Diff_Act_Pred_Revenue "
+            "FROM CATEGORY AS C "
+            "LEFT JOIN ASSIGNED AS A ON C.Category_Name = A.Category_Name "
+            "LEFT JOIN PRODUCT AS P ON A.PID = P.PID "
+            "LEFT JOIN SALE AS S ON P.PID = S.PID "
+            "LEFT JOIN DISCOUNT AS D ON S.Date = D.Date AND S.PID = D.PID "
+            "WHERE C.Category_Name = 'Couches and Sofas' "
+            "GROUP BY P.PID "
+            "HAVING Diff_Act_Pred_Revenue > 5000 OR Diff_Act_Pred_Revenue < -5000 "
+            "ORDER BY Diff_Act_Pred_Revenue DESC; "
+        )
+        report2_res = self.cursor.fetchall()
+        return report2_res
+
     # Report 3_Store Revenue by Year by State: Get Available State List
     def get_state_list(self):
         self.cursor.execute("SELECT DISTINCT State_Location FROM CITY ORDER BY State_Location ASC")
@@ -100,15 +138,33 @@ class SqlHelper(object):
 
     # Report 3_Store Revenue by Year by State
     def report3_store_revenue_by_year_by_state(self, state_location):
-        self.cursor.execute("SELECT STORE.Store_Number, Street_Address, City_Name, YEAR(Date)"
+        self.cursor.execute("SELECT STORE.Store_Number AS Store_ID, Street_Address, City_Name, YEAR(Date) AS Year"
                             ", SUM(IFNULL(Total_Amount, 0)) AS Revenue "
                             "FROM STORE "
                             "LEFT JOIN SALE ON STORE.Store_Number = SALE.Store_Number "
                             "WHERE STORE.State_Location = %s"
                             "GROUP BY STORE.Store_Number, YEAR(Date)"
                             "ORDER BY YEAR(Date) ASC, Revenue DESC;", [state_location])
+
         report3_res = self.cursor.fetchall()
         return report3_res
+
+    # report 4 Outdoor Furniture Revenue: get data set
+    def get_report4(self):
+        self.cursor.execute(
+            "SELECT YEAR(Date) AS Year, SUM(IFNULL(Quantity, 0)) AS Tot_Quantity, "
+            "(SUM(IFNULL(Quantity, 0)) / 365) AS Avg_Quantity, "
+            "SUM(IF(MONTH(Date)=2 AND DAY(Date)=2,1,0) * IFNULL(Quantity, 0)) AS GhDay_Quantity "
+            "FROM CATEGORY AS C "
+            "LEFT JOIN ASSIGNED AS A ON C.Category_Name = A.Category_Name "
+            "LEFT JOIN PRODUCT AS P ON A.PID = P.PID "
+            "LEFT JOIN SALE AS S ON P.PID = S.PID "
+            "WHERE C.Category_Name = 'Outdoor Furniture' "
+            "GROUP BY YEAR(Date) "
+            "ORDER BY YEAR(Date) ASC; "
+        )
+        report4_res = self.cursor.fetchall()
+        return report4_res
 
     # Report 6_Revenue by population
     def report6_revenue_by_population(self):
--- rzhang609/LSRS/reporting/urls.py
+++ rzhang609/LSRS/reporting/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from reporting import views
 from reporting.maintenances import holiday
 from reporting.maintenances import population
-from reporting.reports import report3, report6, report8, report9
+from reporting.reports import report1, report2, report3, report4, report6, report8, report9
 
 urlpatterns = [
     path('', views.index, name='index')
@@ -12,39 +12,22 @@ urlpatterns += [
     path('population/', population.city_population_load, name='population'),
     path('population/get_city_list/', population.get_city_list, name='population_get_city_list'),
     path('population/get_city_population/', population.get_city_population, name='population_get_city_population'),
-]
-
-urlpatterns += [
     path('holiday/', holiday.HolidayList.as_view(), name='holiday'),
     path('holiday/add_holiday/', holiday.add_new_holiday, name='add_holiday')
 ]
 
 urlpatterns += [
-    path(
-        "report8_restaurant_impact_on_category_sales/",
-        report8.GetReport.as_view(),
-        name="report8_restaurant_impact_on_category_sales",
-    )
-]
-
-urlpatterns += [
+    path("report1_category/", report1.GetReport.as_view(), name="report1_category"),
+    path("report2_actual_versus_predicted_revenue_for_couches_and_sofas/",
+         report2.GetReport.as_view(), name="report2_actual_versus_predicted_revenue_for_couches_and_sofas"),
     path('report3_store_revenue_by_year_by_state/', report3.store_revenue,
-         name='report3_store_revenue_by_year_by_state')
+         name='report3_store_revenue_by_year_by_state'),
+    path("report4_outdoor_furniture_revenue/",  report4.GetReport.as_view(),name="report4_outdoor_furniture_revenue"),
+    path('report6_revenue_by_population/', report6.GetReport6.as_view(), name='report6_revenue_by_population'),
+    path("report8_restaurant_impact_on_category_sales/",
+         report8.GetReport.as_view(),name="report8_restaurant_impact_on_category_sales"),
+    path("report9_advertising_campaign_analysis/",
+         report9.GetReport.as_view(),name="report9_advertising_campaign_analysis")
 ]
-
-urlpatterns += [
-    path('report6_revenue_by_population/', report6.GetReport6.as_view(), name='report6_revenue_by_population')
-]
-
-
-urlpatterns += [
-    path(
-        "report9_advertising_campaign_analysis/",
-        report9.GetReport.as_view(),
-        name="report9_advertising_campaign_analysis",
-    )
-]
-
-
 
 
