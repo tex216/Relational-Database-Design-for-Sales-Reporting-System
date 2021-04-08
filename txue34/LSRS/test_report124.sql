7e89d76b2816427d8d66877a8f1514b8e0c360a0
txue34 <txue34@gatech.edu>
2021-04-08 13:59:40 -0400

    bug fixed, final version of report1,2,4

--- txue34/LSRS/test_report124.sql
+++ txue34/LSRS/test_report124.sql
@@ -10,6 +10,9 @@ INSERT INTO `DAY`
 VALUES
 ('2012-02-02'),
 ('2012-06-02'),
+('2012-06-03'),
+('2013-04-28'),
+('2013-04-29'),
 ('2013-09-02');
 
 INSERT INTO STORE
@@ -20,7 +23,8 @@ INSERT INTO PRODUCT
 VALUES
 ('PID_1', 'Product_Name_1', 10),
 ('PID_2', 'Product_Name_2', 20),
-('PID_3', 'Product_Name_3', 7);
+('PID_3', 'Product_Name_3', 7),
+('PID_4', 'Product_Name_4', 400);
 
 INSERT INTO CATEGORY
 VALUES
@@ -33,19 +37,26 @@ INSERT INTO ASSIGNED
 VALUES
 ('PID_1', 'Couches and Sofas'),
 ('PID_2', 'Outdoor Furniture'),
-('PID_3', 'Outdoor Furniture');
+('PID_3', 'Outdoor Furniture'),
+('PID_4', 'Couches and Sofas');
 
 
 INSERT INTO SALE
 VALUES
 ('2012-02-02', 'Store_Number_1', 'PID_2', 10, 80),
-('2012-06-02', 'Store_Number_1', 'PID_1', 5,  100),
+('2012-06-02', 'Store_Number_1', 'PID_1', 50000, 800000),
+('2012-06-03', 'Store_Number_1', 'PID_1', 5000, 80000),
+('2013-04-28', 'Store_Number_1', 'PID_4', 10000, 350000),
+('2013-04-29', 'Store_Number_1', 'PID_4', 1000, 35000),
 ('2013-09-02', 'Store_Number_1', 'PID_3', 100, 20);
 
 INSERT INTO DISCOUNT
 VALUES
 ('2012-02-02', 'PID_2', 8),
-('2013-09-02', 'PID_3', 7);
+('2012-06-02', 'PID_1', 20),
+('2013-09-02', 'PID_3', 7),
+('2013-04-28', 'PID_4', 15);
+
 
 
 
